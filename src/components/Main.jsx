68186105934449b8b4918615ae34413bdeff7a1d
YJSON <1159628561@qq.com>
2017-07-19 13:34:19 +0800

    你好世界

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,22 @@ require('styles/App.less');
 import React from 'react';
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      	<div className="index">
-       		<h1 style={{textAlign:"center",marginTop:"150px",color:"#fff"}}>hello world</h1>
-      	</div>
-    );
-  }
+	constructor(props){
+		super(props);
+	}
+	render() {
+		return (
+		  	<div className="index">
+		   		<h1 style={{textAlign:'center',marginTop:'150px',color:'#fff'}}>{this.props.hello}</h1>
+		   		<h1 style={{textAlign:'center',marginTop:'20px',color:'#fff'}}>{this.props.title}</h1>
+		  	</div>
+		);
+	}
 }
 
 AppComponent.defaultProps = {
+	hello:'hello world',
+	title:'你好，世界！'
 };
 
 export default AppComponent;
