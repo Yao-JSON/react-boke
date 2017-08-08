f80f729deb04d64d5f014361795d216baf010478
姚嘉松 <yjson921305@gmail.com>
2017-08-08 21:00:06 +0800

    简历page01

--- src/components/Resume/resume-page00.jsx
+++ src/components/Resume/resume-page00.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import classnames from 'classnames';
 class ResumePage00 extends React.Component{
   constructor(props){
     super(props);
   }
   render(){
-    return(<div className={this.props.className}>
+    return(<div className={classnames(this.props.className,'resume-book-page01')}>
       <div className='front'>
         <div className='page-root'>
-          0000 front
+          <div className='book-page01-image-container'>
+            <img className='img-responsive' src="../../images/YJSON-me.jpeg" alt=""/>
+          </div>
+          <div className='YJSON-init'>
+            <h1>姚嘉松</h1>
+            <p>WEB前端工程师</p>
+            <p><a href="mailto:YJSON921305@gmail.com">YJSON921305@gmail.com</a></p>
+          </div>
         </div>
       </div>
     </div>);
