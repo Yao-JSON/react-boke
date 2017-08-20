44cd4406c011eedf9ed4956b33d843a2e4706988
姚嘉松 <yjson921305@gmail.com>
2017-08-20 17:25:31 +0800

    API

--- src/sources/blog/project/index.js
+++ src/sources/blog/project/index.js
@@ -1,19 +1,19 @@
 module.exports = [
-  // {
-  // title:'qingful电商批发商城后台',
-  // time:'2017-04-11',
-  // description:'vue+vue-router+vuex+iview 实现电商管理后台',
-  // img:'',
-  // link:'https://github.com/YaoJSON/vue-qingful-waimai-www',
-  // label:['vue','webpack','iview'],
-  // demo:'https://yaojson.github.io/demo/pifa/'
-  // },
+  {
+  title:'qingful电商批发商城后台',
+  time:'2017-04-11',
+  description:'vue+vue-router+vuex+iview 实现电商管理后台',
+  img:'',
+  link:'https://github.com/YAO-JSON/vue-qingful-waimai-www',
+  label:['vue','webpack','iview'],
+  demo:'https://yao-json.github.io/demo/pifa/'
+  },
   {
     title:'画廊应用',
     time:'2017-04-10',
     description:'yeoman + webpack + react + es6/7 + sass + css3 打造画廊应用',
     img:'react',
-    link:'https://github.com/YaoJSON/gallery-by-react',
+    link:'https://github.com/YAO-JSON/gallery-by-react',
     label:['react','webpack'],
     demo:'https://yao-json.github.io/demo/gallery/'
   },
@@ -22,25 +22,25 @@ module.exports = [
     time:'2017-04-08',
     description:'使用Redux管理你的React应用',
     img:'react',
-    link:'https://github.com/YaoJSON/redux-example',
+    link:'https://github.com/YAO-JSON/redux-example',
     label:['react','redux','webpack'],
     demo:'#'
   },
-  // {
-  //   title:'批发商城API',
-  //   time:'2017-04-01',
-  //   description:'使用koa+kenxJS+APIdoc 开发的零售商城API',
-  //   img:'node',
-  //   link:'https://github.com/YaoJSON/qingful-pifa-api',
-  //   label:['node','knexJS','webpack','koahub','mocha'],
-  //   demo:'https://yao-json.github.io/demo/api/pifa/apidoc/'
-  // },
+  {
+    title:'批发商城API',
+    time:'2017-04-01',
+    description:'使用koa+kenxJS+APIdoc 开发的零售商城API',
+    img:'node',
+    link:'https://github.com/YAO-JSON/qingful-pifa-api',
+    label:['node','knexJS','webpack','koahub','mocha'],
+    demo:'https://yao-json.github.io/demo/api/pifa/apidoc/'
+  },
   {
     title:'Vue.js高仿饿了么',
     time:'2017-03-21',
     description:'Vue.js高仿饿了么外卖App课程源码',
     img:'vue',
-    link:'https://github.com/YaoJSON/vue-ele',
+    link:'https://github.com/YAO-JSON/vue-ele',
     label:['vue','vue-cli','webpack','es6/7','style'],
     demo:'https://yao-json.github.io/demo/ele/'
   },
@@ -52,22 +52,21 @@ module.exports = [
     link:'https://github.com/YaoJSON/vue-ele',
     label:['vue','vue-cli','webpack','es6/7','style'],
     demo:'https://yao-json.github.io/demo/ele-admin/'
+  },
+  {
+    title:'qingful-app-wxa',
+    time:'2017-03-20',
+    description:'微信小程序 APP 打印',
+    img:'wxa',
+    link:'https://github.com/YAO-JSON/qingful-app-wxa',
+    label:['wxa']
+  },
+  {
+    title:'qingful-app-wxa',
+    time:'2017-03-20',
+    description:'微信小程序 普通商家店铺',
+    img:'wxa',
+    link:'https://github.com/YAOJSON/duodian-wxa',
+    label:['wxa']
   }
-  //,
-  // {
-  //   title:'qingful-app-wxa',
-  //   time:'2017-03-20',
-  //   description:'微信小程序 APP 打印',
-  //   img:'wxa',
-  //   link:'https://github.com/YaoJSON/qingful-app-wxa',
-  //   label:['wxa']
-  // },
-  // {
-  //   title:'qingful-app-wxa',
-  //   time:'2017-03-20',
-  //   description:'微信小程序 普通商家店铺',
-  //   img:'wxa',
-  //   link:'https://github.com/YaoJSON/duodian-wxa',
-  //   label:['wxa']
-  // }
 ];
